2039471addf3ed7e23e45ded07b8a47300251aef
xsepower <savchenko.kgu@gmail.com>
2020-06-08 15:17:19 +0300

    Лабораторная работа
    №5

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double a = 2, b = 6, c = 9;
+            string actError, expError = "Нет действительных корней";
+            double[] expResult = new double[0];
+            double[] actResult = Class1.Result(a, b, c, out actError);
+            Assert.AreEqual(expError, actError, "Полученное сообщение (" + actError + ") об ошибке не совпадает с ожиданием (" + expError + ")");
+            Assert.AreEqual(expResult.Length, actResult.Length);
+            for (int i = 0; i < expResult.Length; i++)
+            {
+                Assert.AreEqual(expResult[i], actResult[i]);
+            }
+        }
+    }
+}
